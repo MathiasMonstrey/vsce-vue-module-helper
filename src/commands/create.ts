82b1b4b869411707eb423fc081293103b223b579
Mathias Monstrey <me@mathiasmonstrey.be>
2019-12-09 23:30:27 +0100

    implementing templates

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,107 +1,75 @@
 import * as fs from 'fs';
 import * as vscode from 'vscode';
+import * as path from 'path';
 import { camelCase, startCase } from 'lodash';
 import { window } from 'vscode';
 
-export default async (moduleName: string, fsPath: string) => {
-    if (moduleName) {
-        const normalizedName = normalizeName(moduleName);
-        const modulePath = `${fsPath}/${normalizedName}`;
-        if (!fs.existsSync(modulePath)) {
-            fs.mkdirSync(modulePath);
+export default async (context: vscode.ExtensionContext, name: string, fsPath: string, module: boolean) => {
+    if (name) {
+        const normalizedName = normalizeName(name, module);
+        const path = `${fsPath}/${normalizedName}`;
+        const testPath = `${path}/tests/`;
+
+        if (!fs.existsSync(path)) {
+            fs.mkdirSync(path);
         }
 
-        if (fs.existsSync(modulePath)) {
-            fs.writeFile(`${modulePath}/${moduleName}.vue`, getVue(moduleName), () => { });
-            fs.writeFile(`${modulePath}/${moduleName}.ts`, getTypescript(moduleName), () => { });
-            fs.writeFile(`${modulePath}/${moduleName}.html`, getHtml(moduleName), () => { });
-            fs.writeFile(`${modulePath}/${moduleName}-routes.ts`, getRoute(moduleName), () => { });
-
-            fs.mkdir(`${modulePath}/tests/`, () => {
-                fs.writeFile(`${modulePath}/tests/${moduleName}.spec.ts`, getUnitTest(moduleName), () => { });
+        if (fs.existsSync(path)) {
+            writeTemplate(replaceValues(readFile('template-vue.txt', context), dictGen(name)), name, 'vue', path);
+            writeTemplate(replaceValues(readFile('template-ts.txt', context), dictGen(name)), name, 'ts', path);
+            writeTemplate(replaceValues(readFile('template-html.txt', context), dictGen(name)), name, 'html', path);
+            writeTemplate(Promise.resolve(''), name, 'scss', path);
+            if (module) {
+                writeTemplate(replaceValues(readFile('template-route.txt', context), dictGen(name)), `${name}-routes`, 'ts', path);
+            }
+            fs.mkdir(testPath, () => {
+                writeTemplate(replaceValues(readFile('template-unittest.txt', context), dictGen(name)), `${name}-spec`, 'ts', testPath);
             });
         }
     } else {
         window.showWarningMessage("Provide a valid module name");
     }
+};
+
+function dictGen(name: string) {
+    return { name, pascalCasedName: getPascalName(name) };
 }
 
-function normalizeName(moduleName: string) {
-    if (vscode.workspace.getConfiguration().get('conf.resource.addModuleToName')) {
-        if (moduleName.endsWith('-module')) {
-            return moduleName;
+function getPascalName(name: string) {
+    return startCase(camelCase(name)).replace(/ /g, '');
+}
+
+function normalizeName(name: string, module: boolean) {
+    if (module && vscode.workspace.getConfiguration().get('vmh.addModuleToName')) {
+        if (name.endsWith('-module')) {
+            return name;
         } else {
-            return `${moduleName}-module`;
+            return `${name}-module`;
         }
     } else {
-        return moduleName;
+        return name;
     }
 }
 
-function getVue(moduleName: string) {
-    return `<template src="./${moduleName}.html">
-</template>
-
-<script lang="ts" src="./${moduleName}.ts">
-</script>
-
-<style lang="scss">
-@import "./${moduleName}.scss";
-</style>
-	`;
+function writeTemplate(template: Promise<string>, name: string, extension: string, path: string) {
+    template.then((parsedTemplate) => {
+        fs.writeFile(`${path}/${name}.${extension}`, parsedTemplate, () => { });
+    });
 }
 
-function getTypescript(moduleName: string) {
-    const pascalCasedName = startCase(camelCase(moduleName)).replace(/ /g, '');
-    return `import { Component, Prop, Vue } from 'vue-property-decorator';
-
-@Component
-export default class ${pascalCasedName} extends Vue {
-
-}
-`;
+function readFile(filename: string, context: vscode.ExtensionContext): Promise<string> {
+    return new Promise((resolve, reject) => {
+        fs.readFile(vscode.Uri.file(path.join(context.extensionPath, 'src', 'templates', filename)).fsPath, 'utf8', (err, data) => {
+            err ? reject(err) : resolve(data)
+        });
+    });
 }
 
-function getRoute(moduleName: string) {
-    return `import { RouteConfig } from 'vue-router';
-
-export default [{
-	path: '/${moduleName}',
-	name: '${moduleName}',
-	component: () => import('./${moduleName}'),
-}] as RouteConfig[];
-
-`;
-}
-
-function getHtml(moduleName: string) {
-    return `<div>
-	${moduleName} created!
-</div>
-`;
-}
-
-function getUnitTest(moduleName: string) {
-    const pascalCasedName = startCase(camelCase(moduleName)).replace(/ /g, '');
-    return `import { shallowMount, createLocalVue, Wrapper } from '@vue/test-utils';
-import ${pascalCasedName} from '../${moduleName}';
-import WebComponents from '@/plugins/webComponents';
-
-describe('${pascalCasedName}', () => {
-	const localVue = createLocalVue();
-	localVue.use(WebComponents);
-
-	let wrapper: Wrapper<${pascalCasedName}>;
-	beforeEach(() => {
-		wrapper = shallowMount(${pascalCasedName}, {
-			propsData: {},
-			localVue,
-		});
-	});
-
-	it('Test written for ${moduleName}', () => {
-		throw new Error('No test written');
-	});
-});
-`;
+function replaceValues(filePromise: Promise<string>, replaceDictionary: { [index: string]: string }) {
+    return filePromise.then((fileData) => {
+        Object.keys(replaceDictionary).forEach((key) => {
+            fileData = fileData.replace(new RegExp(`{${key}}`, 'g'), replaceDictionary[key]);
+        })
+        return fileData;
+    });
 }
--- src/extension.ts
+++ src/extension.ts
@@ -5,17 +5,29 @@ export function activate(context: vscode.ExtensionContext) {
 
 	vscode.commands.executeCommand('setContext', 'createIsActive', true);
 
-	vscode.commands.registerCommand('extension.create', async (params) => {
+	vscode.commands.registerCommand('vmh.addModule', async (params) => {
 		const moduleName = await vscode.window.showInputBox({
 			value: '',
 			placeHolder: 'Provide a name for the module'
 		});
 		if (moduleName && params) {
-			create(moduleName, params.fsPath);
+			create(context, moduleName, params.fsPath, true);
 		}else{
 			vscode.window.showWarningMessage("Can't add module");
 		}
 	});
+
+	vscode.commands.registerCommand('vmh.addComponent', async(params) => {
+		const moduleName = await vscode.window.showInputBox({
+			value: '',
+			placeHolder: 'Provide a name for the component'
+		});
+		if (moduleName && params) {
+			create(context, moduleName, params.fsPath, false);
+		}else{
+			vscode.window.showWarningMessage("Can't add component");
+		}
+	})
 }
 
 
